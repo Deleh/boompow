4fd1294633bad4406e512502b04dfa9171f16120
Denis Lehmann <denis@opaque.tech>
2021-10-03 12:48:17 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -9,22 +9,71 @@
       with nixpkgs.lib;
       {
         options = {
-          enable = mkEnableOption "Enable BoomPow client";
-          
-          user = mkOption {
+
+          cpuThreads = mkOption {
+            type = types.int;
+            default = 1;
+            description = ''
+              Specifies how many CPU threads to use.
+
+              This option os only applied if 'mode' is set to 'cpu'.
+            '';
+          };
+
+          gpuAddress = mkOption {
             type = types.str;
-            default = "bpow";
-            description = "User under which the client runs";
+            default = "0:0";
+            description = ''
+              Specifies which GPU(s) to use in the form <PLATFORM:DEVICE:THREADS>...
+              THREADS is optional and defaults to 1048576.
+
+              This option is only applied if 'mode' is set to 'gpu'.
+            '';
           };
 
           group = mkOption {
             type = types.str;
             default = "bpow";
-            description = "Group under which the client runs";
+            description = "Group under which the BoomPow client and Nano work server run.";
+          };
+
+          mode = mkOption {
+            type = types.enum [ "cpu" "gpu" ];
+            default = "gpu";
+            description = ''
+              Run the Nano work server in CPU or GPU mode.
+
+              Use the options 'gpuAddress' and 'cpuThreads' to configure the modes.
+            '';
+          };
+
+          port = mkOption {
+            type = types.int;
+            default = 7000;
+            description = "Local port of the Nano work server.";
+          };
+
+          user = mkOption {
+            type = types.str;
+            default = "bpow";
+            description = "User under which the BoomPow client and Nano work server run.";
+          };
+
+          walletAddress = mkOption {
+            type = types.str;
+            description = "Banano wallet address which will receive the payments.";
+          };
+
+          workType = mkOption {
+            type = types.enum [ "any" "ondemand" "precache" ];
+            default = "any";
+            description = "Work type, one of 'any', 'ondemand' or 'precache'.";
           };
         };
 
-        config = mkIf config.enable {
+        config = {
+
+          # Systemd service for bpow-client
           systemd.services.bpow-client = {
             description = "BoomPow client";
             after = ["network.target"];
@@ -37,21 +86,70 @@
               Restart = "always";
               RestartSec = "5s";
               PermissionsStartOnly = true;
-        
               ExecStart = ''
-                while true; do echo running; sleep 1; done
+                ${self.packages.${config.nixpkgs.localSystem.system}.bpow-client}/bin/bpow-client \
+                  --payout ${config.walletAddress} \
+                  --work ${config.workType} \
+                  --worker_uri 127.0.0.1:${toString config.port} \
+                  --limit-logging
               '';
             };
           };
+
+          # Systemd service for nano-work-server
+          systemd.services.nano-work-server = {
+            description = "nano work server";
+            after = ["network.target"];
+            wantedBy = ["multi-user.target"];
+            restartIfChanged = true;
+            serviceConfig = {
+              type = "simple";
+              User = config.user;
+              Group = config.group;
+              Restart = "always";
+              RestartSec = "5s";
+              PermissionsStartOnly = true;
+              ExecStart = ''
+                ${self.packages.${config.nixpkgs.localSystem.system}.nano-work-server}/bin/nano-work-server \
+                   ${if config.mode == "gpu" then "--gpu ${config.gpuAddress}" else "--cpu-threads ${toString config.cpuThreads}"} \
+                   --listen-address 127.0.0.1:${toString config.port}
+              '';
+            };
+          };
+
+          # User and group
+          users.users = mkIf (config.user == "bpow") {
+            bpow = {
+              group = config.group;
+              isSystemUser = true;
+            };
+          };
+          users.groups = mkIf (config.group == "bpow") {
+            bpow = {};
+          };
+
         };
       };
 
   } // (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+    (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
         {
+
+          # bpow-client
+          packages.bpow-client =
+            pkgs.python3Packages.buildPythonApplication rec {
+              name = "bpow-client";
+              src = "${self}/client";
+              propagatedBuildInputs = with pkgs.python3Packages; [
+                amqtt
+                aiohttp
+                requests
+                setuptools
+              ];
+            };
 
           # nano-work-server
           packages.nano-work-server =
@@ -69,22 +167,9 @@
               '';
             };
 
-          # bpow-client
-          packages.bpow-client =
-            pkgs.python3Packages.buildPythonApplication rec {
-              name = "bpow-client";
-              src = "${self}/client";
-              propagatedBuildInputs = with pkgs.python3Packages; [
-                amqtt
-                aiohttp
-                requests
-                setuptools
-              ];
-            };
-
           # Default package
-          defaultPackage = self.packages.${system}.nano-work-server;
+          defaultPackage = self.packages.${system}.bpow-client;
         }
 
-      ));
+    ));
 }
